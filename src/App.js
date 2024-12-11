f230bbb07126a01b64e2dd2b9c6c64d41c9ed8d1
Ryan Gascoigne-Jones <rtg204@exeter.ac.uk>
2024-12-11 19:41:49 +0000

    base website created

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import brand_logo from './assets/BGfitness_logo_dark_mode_cropped.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={brand_logo} alt="business logo"></img>
         <nav>
-          <a href="#about">About</a>
+          <a href="#about">About me</a>
           <a href="#services">Services</a>
+          <a href="#reviews">Reviews</a>
           <a href="#contact">Contact</a>
         </nav>
       </header>
-      <section id="about">
-        <h1>About Us</h1>
-        <p>We are a small business specializing in XYZ.</p>
-      </section>
-      <section id="services">
-        <h1>Our Services</h1>
-        <p>Details about services offered.</p>
-      </section>
-      <section id="contact">
-        <h1>Contact</h1>
-        <p>Email: example@business.com</p>
-      </section>
+      <div class="container">
+        <section id="about">
+          <h1>About Me</h1>
+          <div class="modal-body row">
+            <div class="col-md-6">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mauris ipsum, molestie id gravida vitae, porta non ipsum. Mauris a diam eu lacus scelerisque blandit nec ut erat. Vestibulum nec tincidunt nulla. Nunc vel enim libero. Fusce lectus nunc, porttitor facilisis leo eu, varius sollicitudin dui. Proin eget sollicitudin lectus. Morbi gravida a mi eget vehicula. Aenean consectetur tellus at nulla hendrerit efficitur. Nullam gravida consequat justo vel iaculis. 
+              </p>
+            </div>
+            <div class="col-md-6">
+              Picture
+            </div>
+          </div>
+          <p>
+            Proin tristique placerat aliquam. Maecenas interdum nec lectus malesuada scelerisque. In vitae massa eu dolor porta ultrices. Sed molestie, mi accumsan maximus rhoncus, neque eros pellentesque ipsum, sit amet porttitor sem enim sed augue. Sed faucibus lorem nibh, finibus interdum massa blandit et. Praesent scelerisque turpis sed aliquet sodales. Praesent ultricies non sem eget gravida. Phasellus rutrum odio et tristique mattis. Nulla purus augue, posuere eget sapien et, ultricies sollicitudin lorem. Sed in magna at risus vehicula dignissim non sollicitudin mi. 
+          </p>
+        </section>
+        <section id="services">
+          <h1>Services</h1>
+          <p>Personal training</p>
+        </section>
+        <section id="reviews">
+          <h1>Reviews</h1>
+          <p>Review 1 (quote box)</p>
+        </section>
+        <section id="contact">
+          <h1>Contact</h1>
+          <p>Email: babs@bgfitness.co.uk</p>
+          <p>Mobile: (+44) 07977 211484</p>
+        </section>
+      </div>
     </div>
   );
 }
