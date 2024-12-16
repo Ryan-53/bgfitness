e2e09a9c6acd3db6a665a170dc07c2e0c5c62d3e
Ryan Gascoigne-Jones <rtg204@exeter.ac.uk>
2024-12-16 17:49:16 +0000

    Footer sticks to bottom of content

--- src/App.js
+++ src/App.js
@@ -24,40 +24,41 @@ function App() {
         </nav>
       </header>
       <div className="container">
-        <div className="quick-contact">
-          {/* TODO: Links */}
-          <p>
-            <i class="fa-solid fa-envelope"></i>
-            &nbsp; babs@bgfitness.co.uk &nbsp;|&ensp;
-            <i class="fa-solid fa-phone"></i>
-            &nbsp; 07977 211484
-          </p>
-        </div>
-        {/* TODO: Add in picture to top-right of section*/}
-        <section id="about">
-          <h1>About Me</h1>
-          <div className="modal-body row">
-            <div className="col-md-6">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mauris ipsum, molestie id gravida vitae, porta non ipsum. Mauris a diam eu lacus scelerisque blandit nec ut erat. Vestibulum nec tincidunt nulla. Nunc vel enim libero. Fusce lectus nunc, porttitor facilisis leo eu, varius sollicitudin dui. Proin eget sollicitudin lectus. Morbi gravida a mi eget vehicula. Aenean consectetur tellus at nulla hendrerit efficitur. Nullam gravida consequat justo vel iaculis. 
-              </p>
-            </div>
+        <div id="content-wrap">
+          <div className="quick-contact">
+            {/* TODO: Links */}
+            <p>
+              <i class="fa-solid fa-envelope"></i>
+              &nbsp; babs@bgfitness.co.uk &nbsp;|&ensp;
+              <i class="fa-solid fa-phone"></i>
+              &nbsp; 07977 211484
+            </p>
           </div>
-          <p>
-            Proin tristique placerat aliquam. Maecenas interdum nec lectus malesuada scelerisque. In vitae massa eu dolor porta ultrices. Sed molestie, mi accumsan maximus rhoncus, neque eros pellentesque ipsum, sit amet porttitor sem enim sed augue. Sed faucibus lorem nibh, finibus interdum massa blandit et. Praesent scelerisque turpis sed aliquet sodales. Praesent ultricies non sem eget gravida. Phasellus rutrum odio et tristique mattis. Nulla purus augue, posuere eget sapien et, ultricies sollicitudin lorem. Sed in magna at risus vehicula dignissim non sollicitudin mi. 
-          </p>
-        </section>
-        <section id="services">
-          <h1>Services</h1>
-          <p>Personal training</p>
-        </section>
-        <section id="reviews">
-          <h1>Testimonials</h1>
-          <Reviews reviews={reviews}/>
-        </section>
-      {/* TODO: Fix whitespace gap between reviews and footer (issue may be in reviews section) */}
-      </div>
-        <div className="site-footer" id="contact">
+          {/* TODO: Add in picture to top-right of section*/}
+          <section id="about">
+            <h1>About Me</h1>
+            <div className="modal-body row">
+              <div className="col-md-6">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mauris ipsum, molestie id gravida vitae, porta non ipsum. Mauris a diam eu lacus scelerisque blandit nec ut erat. Vestibulum nec tincidunt nulla. Nunc vel enim libero. Fusce lectus nunc, porttitor facilisis leo eu, varius sollicitudin dui. Proin eget sollicitudin lectus. Morbi gravida a mi eget vehicula. Aenean consectetur tellus at nulla hendrerit efficitur. Nullam gravida consequat justo vel iaculis. 
+                </p>
+              </div>
+            </div>
+            <p>
+              Proin tristique placerat aliquam. Maecenas interdum nec lectus malesuada scelerisque. In vitae massa eu dolor porta ultrices. Sed molestie, mi accumsan maximus rhoncus, neque eros pellentesque ipsum, sit amet porttitor sem enim sed augue. Sed faucibus lorem nibh, finibus interdum massa blandit et. Praesent scelerisque turpis sed aliquet sodales. Praesent ultricies non sem eget gravida. Phasellus rutrum odio et tristique mattis. Nulla purus augue, posuere eget sapien et, ultricies sollicitudin lorem. Sed in magna at risus vehicula dignissim non sollicitudin mi. 
+            </p>
+          </section>
+          <section id="services">
+            <h1>Services</h1>
+            <p>Personal training</p>
+          </section>
+          <section id="reviews">
+            <h1>Testimonials</h1>
+            <Reviews reviews={reviews}/>
+          </section>
+        </div>
+        {/* TODO: Fix whitespace gap between reviews and footer / footer and bottom of page (issue may be in reviews section) */}
+        <footer className="site-footer" id="contact">
           <h1>Contact Info</h1>
           {/* TODO: Link to email */}
           <p>
@@ -73,8 +74,11 @@ function App() {
             <i class="fa-solid fa-location-dot"></i>
             &nbsp; Audley, Stoke-on-Trent
           </p>
-        </div>
+        </footer>
+      </div>
+        
     </div>
+    
   );
 }
 
